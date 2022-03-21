ac0e33e45c021316c7f3c158db82aa2f92f26a16
priya-jagyasi011 <101853524+priya-jagyasi011@users.noreply.github.com>
2022-03-21 11:58:50 +0530

    Create file2.js

---
+++ file2.js
@@ -0,0 +1,15 @@
+import { createCustomElement, actionTypes } from '@servicenow/ui-core';
+import { Fragment } from '@servicenow/ui-renderer-snabbdom';
+import "@servicenow/now-loader";
+import "@servicenow/now-image";
+import "@servicenow/now-checkbox";
+import "@servicenow/now-text-link";
+
+
+function generatePaginationConfig(paginationData) {
+	return {
+		...PAGINATION_CONTROL_DEFAULTS,
+		pagesOverride: Math.ceil(paginationData.total / PAGINATION_LIMIT),
+		selectedPage: paginationData.selectedPage,
+		total: paginationData.total
+	};
